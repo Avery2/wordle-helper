9085dd0ac2d29a3d71c8480466edc88a204b5e00
Avery Chan <JustinAveryChan@gmail.com>
2022-02-09 16:27:54 -0600

    feat: helper works minmally

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # wordle-helper
 
 Possible words list from [3b1b](https://github.com/3b1b/videos/tree/master/_2022/wordle).
+
+3 parameters must be included:
+
+- Known: Characters with known positions as a string of length 5 with `.` as placeholders such as `..xy.`.
+- Included: Characters that are included in the word but in unkown location in the form `xyz`.
+- Excluded: Characters that are excluded in the word in the form `xyz`.
---
+++ helper.py
@@ -0,0 +1,38 @@
+import re
+import sys
+
+
+if __name__ == '__main__':
+    # read file
+    with open("possible_words.txt") as possibleWords:
+        words = [x.strip() for x in possibleWords.readlines()]
+
+    # command line parsing
+    if len(sys.argv) != 4:
+        print("usage: helper known included excluded")
+        exit(1)
+
+    known = sys.argv[1]
+    included = set(sys.argv[2])
+    excluded = set(sys.argv[3])
+
+    if len(known) != 5:
+        print(f"arg known must be of length 5 but was of length {len(known)}")
+        exit(1)
+
+    # print(f"{known=} {included=} {excluded=}")
+
+    # filter possible words
+    chars = set('abcdefghijklmnopqrstuvwxyz')
+
+    re_str = known
+    chars_ = ''.join(list(chars - excluded))
+    print(f"{chars_=}")
+    re_str = known.replace('.', f'[{chars_}]')
+    r = re.compile(re_str)
+    matches = list(filter(r.match, words))
+    matches = list(filter(lambda x: False not in [e in x for e in included], matches))
+    
+    print(f"Show {len(matches)} possible words? [y/n] ", end='')
+    if input().strip().lower() in ('yes', 'y'):
+        print(*matches, sep="\n")
