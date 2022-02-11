366f11109a45529d604f3e600afb80bde30da192
Avery Chan <JustinAveryChan@gmail.com>
2022-02-11 01:18:08 -0600

    feat: can exclude characters from specific positions

--- helper.py
+++ helper.py
@@ -2,6 +2,12 @@ import re
 import sys
 
 
+def allowed_positiions_only(word, excluded_positions):
+    for ch, ex in zip(word, excluded_positions):
+        if ch in ex:
+            return False
+    return True
+
 if __name__ == '__main__':
     # read file
     with open("possible_words.txt") as possibleWords:
@@ -15,30 +21,32 @@ if __name__ == '__main__':
     known_positions = sys.argv[1]
     included_characters = set(sys.argv[2])
     excluded_characters = set(sys.argv[3])
+    excluded_positions = None
 
     if len(sys.argv) > 4:
         excluded_positions = sys.argv[4]
         if excluded_positions.count("-") != 4:
             print(f"arg excluded_positions ({excluded_positions}) must be of the form x-y-x-- with exactly 4 -")
             exit(1)
+        excluded_positions = excluded_positions.split('-')
 
     if len(known_positions) != 5:
         print(f"arg known_positions ({known_positions=} must be of length 5 but was of length {len(known_positions)}")
         exit(1)
 
-    if excluded_positions:
-        print(f"excluded_positions: {excluded_positions}")
-
     # filter possible words
     all_possible_chars = set('abcdefghijklmnopqrstuvwxyz')
-    # exclude excluded characters
     possible_chars = ''.join(list(all_possible_chars - excluded_characters))
     re_str = known_positions.replace('-', f'[{possible_chars}]')
     r = re.compile(re_str)
 
+    # exclude excluded characters
     matches = list(filter(r.match, words))
     # require included characters
     matches = list(filter(lambda x: False not in [e in x for e in included_characters], matches))
+    # filter excluded positions
+    if excluded_positions:
+        matches = list(filter(lambda x: allowed_positiions_only(x, excluded_positions), matches))
     
     # output to user
     print(f"Show {len(matches)} possible words? [y/n] ", end='')
