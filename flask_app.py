7c96aaba4b6ff9b27cbc387e96cf82207644ec5e
Avery2 <justinaverychan@gmail.com>
2022-10-22 00:33:21 -0700

    fix: put text IN the button

--- flask_app.py
+++ flask_app.py
@@ -10,4 +10,4 @@ def hello_world():
     return '''<h1>Wordle bot</h1>
 
     <p>"Make guess (1), use utility (2), or quit (q): "</p>
-    <button>(1)</button>(2)<button></button>(q)<button></button>'''
+    <button>(1)</button><button>(2)</button><button>(q)</button>'''
