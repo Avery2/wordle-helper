772b90c980fdad0de15c60a7434c23405c2fab98
Avery2 <justinaverychan@gmail.com>
2022-10-22 00:32:30 -0700

    feat: emulate first input

--- flask_app.py
+++ flask_app.py
@@ -7,4 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello from Flask! -- Wordle bot</h1><button>test button</button>'
+    return '''<h1>Wordle bot</h1>
+
+    <p>"Make guess (1), use utility (2), or quit (q): "</p>
+    <button>(1)</button>(2)<button></button>(q)<button></button>'''
