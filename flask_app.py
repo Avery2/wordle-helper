7254303a819c9f54ef4507b5309c8fd2b2e5903c
Avery2 <justinaverychan@gmail.com>
2022-10-22 00:42:06 -0700

    feat: simplify output

--- flask_app.py
+++ flask_app.py
@@ -5,14 +5,8 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 
-# @app.route('/')
-# def hello_world():
-#     return '''<h1>Wordle bot</h1>
-
-#     <p>"Make guess (1), use utility (2), or quit (q): "</p>
-#     <button>(1)</button><button>(2)</button><button>(q)</button>'''
-
 anchor = '<!-- output anchor -->'
+
 
 @app.route('/')
 def my_form():
@@ -23,6 +17,6 @@ def my_form():
 def my_form_post():
     text = request.form['text']
     processed_text = text.upper()
-    response_text = f'<p>Your text was: </p><p>{processed_text}</p>'
+    response_text = f'<p>{processed_text}</p>'
 
     return render_template('my-form.html').replace(anchor, response_text)
